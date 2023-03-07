2ba557e4241938c055c4d5d41badd65bf7b18452
Matthew <matthewbowyer17@gmail.com>
2023-03-07 23:56:07 +0000

    Prototype

---
+++ ScrummersUnityProjects/Assets/UiManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.UI;
+public class UiManager : MonoBehaviour
+{
+    public GameObject MainMenu, SimOnly100gb;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SimOnly100gb.SetActive(false);
+    }
+
+    public void SimOnly100gbEnabled()
+    {
+        MainMenu.SetActive(false);
+        SimOnly100gb.SetActive(true);
+    }
+
+    public void MainMenuEnable()
+    {
+        SimOnly100gb.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
