9ef1331abf2cf8a5e45c8d9b55fbf98b19c7dc40
Omar Latreche <omar1933@hotmail.co.uk>
2023-03-30 21:14:09 +0100

    Grid view scrollfix

--- ScrummersUnityProjects/Assets/Scripts/PopulateProducts.cs
+++ ScrummersUnityProjects/Assets/Scripts/PopulateProducts.cs
@@ -6,13 +6,18 @@ using TMPro;
 
 public class PopulateProducts : MonoBehaviour
 {
-    [SerializeField] private GridLayoutGroup gridLayoutGroup;
-    private string[,] productInfo;
+    [SerializeField] private GridLayoutGroup phoneGridLayoutGroup;
+    [SerializeField] private GridLayoutGroup simOnlyGridLayoutGroup;
+    [SerializeField] private GridLayoutGroup accessoriesGridLayoutGroup;
+    [SerializeField] private GridLayoutGroup tabletsGridLayoutGroup;
+    [SerializeField] private GridLayoutGroup tvAndBroadbandGridLayoutGroup;
     [SerializeField] private GameObject itemPrefab;
 
     void Awake()
-    {
-        string[,] productInfo = new string[18, 3] {
+    {        
+        string template = "{0} {1}, {2}";
+
+        string[,] phoneInfo = new string[18, 3] {
             {"iPhone","X","64gb"},
             {"iPhone","XR","64gb"},
             {"iPhone","14","128gb"},
@@ -32,15 +37,45 @@ public class PopulateProducts : MonoBehaviour
             {"Sony","Xperia 5 III","128gb"},
             {"LG","Velvet","128gb"}
         };
-        string template = "{0} {1}, {2}";
 
-        for (int row = 0; row < productInfo.GetLength(0); row++) { // iterate through rows
+        for (int row = 0; row < phoneInfo.GetLength(0); row++) { // iterate through rows
             string[] product = new string[3];
-            product[0] = productInfo[row, 0];
-            product[1] = productInfo[row, 1];
-            product[2] = productInfo[row, 2];
+            product[0] = phoneInfo[row, 0];
+            product[1] = phoneInfo[row, 1];
+            product[2] = phoneInfo[row, 2];
             GameObject newItem = Instantiate(itemPrefab);
-            newItem.transform.SetParent(gridLayoutGroup.transform);
+            newItem.transform.SetParent(phoneGridLayoutGroup.transform);
+            newItem.GetComponentInChildren<TMP_Text>().text = string.Format(template, product);
+        }
+
+        string[,] simInfo = new string[17, 3] {
+            {"Vodafone","SIM Only","10GB"},
+            {"O2","SIM Only","20GB"},
+            {"Three","SIM Only","Unlimited data"},
+            {"EE","SIM Only","15GB"},
+            {"BT Mobile","SIM Only","30GB"},
+            {"Virgin Media","SIM Only","20GB"},
+            {"Sky Mobile","SIM Only","25GB"},
+            {"Plusnet Mobile","SIM Only","10GB"},
+            {"iD Mobile","SIM Only","20GB"},
+            {"Tesco Mobile","SIM Only","15GB"},
+            {"Asda Mobile","SIM Only","20GB"},
+            {"Smarty","SIM Only","Unlimited data"},
+            {"giffgaff","SIM Only","40GB"},
+            {"VOXI","SIM Only","20GB"},
+            {"Lebara","SIM Only","10GB"},
+            {"Lycamobile","SIM Only","15GB"},
+            {"Superdrug Mobile","SIM Only","10GB"}
+        };
+
+        for (int row = 0; row < simInfo.GetLength(0); row++)
+        { // iterate through rows
+            string[] product = new string[3];
+            product[0] = simInfo[row, 0];
+            product[1] = simInfo[row, 1];
+            product[2] = simInfo[row, 2];
+            GameObject newItem = Instantiate(itemPrefab);
+            newItem.transform.SetParent(simOnlyGridLayoutGroup.transform);
             newItem.GetComponentInChildren<TMP_Text>().text = string.Format(template, product);
         }
     }
