5e3aa340739db6eab0f832236d279e7fe9414e09
Omar Latreche <omar1933@hotmail.co.uk>
2023-03-30 23:00:58 +0100

    Organisation

---
+++ ScrummersUnityProjects/Assets/Scripts/BasketItems.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketItems : MonoBehaviour
+{
+    [SerializeField] public List<GameObject> items;
+
+}
