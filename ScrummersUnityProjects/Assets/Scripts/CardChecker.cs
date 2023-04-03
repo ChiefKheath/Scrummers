54a7a22890a2cbea1971e798f101b6b19b8c004c
ChiefKheath <kh1469s@gre.ac.uk>
2023-04-03 16:40:06 +0100

    Finishing up the payment screen

---
+++ ScrummersUnityProjects/Assets/Scripts/CardChecker.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardChecker : MonoBehaviour
+{
+    public TMP_InputField CardNumberField;
+    public Button finalise;
+    public TMP_InputField CVCField;
+
+    public void CardNumberCheck()
+    {
+        if (CardNumberField.text.Length < 16)
+        {
+            Debug.Log("tooshort");
+        }
+        else if (CardNumberField.text.Length == 16)
+        {
+            Debug.Log("Perfecto");
+        }
+    }
+    public void CVCCheck()
+    {
+        if (CVCField.text.Length < 3)
+        {
+            Debug.Log("tooshort");
+        }
+        else if (CVCField.text.Length == 3)
+        {
+            Debug.Log("Perfecto");
+        }
+    }
+
+    public void FinalisePayment()
+    {
+        if (CardNumberField.text.Length == 16 && CVCField.text.Length == 3)
+        {
+
+        }
+    }
+}
